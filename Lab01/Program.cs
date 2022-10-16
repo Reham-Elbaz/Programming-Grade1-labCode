134ba66422b97942fd192b6ba8eed6370c5adb23
Reham-Elbaz <50484754+Reham-Elbaz@users.noreply.github.com>
2022-10-16 22:53:09 +0200

    Add project files.

---
+++ Lab01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lab01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // To print some statements on multiple lines
+            Console.WriteLine("I'm Reham.");
+            Console.WriteLine("I'm 22 years old.");
+            Console.WriteLine("I'm student at fcis.");
+
+
+            // To print on a single line
+            Console.Write("This will be");
+            Console.Write("printed on the same line");
+
+
+            // Printing on the same line using special character
+            Console.Write("I'm Reham.\nI'm 22 years old.\nI'm student at fcis.");
+        }
+    }
+}
