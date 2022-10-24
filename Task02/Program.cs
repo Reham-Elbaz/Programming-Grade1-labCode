f6bac5d475f814f1d2fe8b7f5052e61ea4f9d5de
Reham-Elbaz <50484754+Reham-Elbaz@users.noreply.github.com>
2022-10-24 23:37:43 +0200

    2nd task

---
+++ Task02/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * This program is to let the user enter salary
+             * and then calculate the net salart after applying the tax of 10,
+             * the show them the before and after.
+             */
+
+
+            Console.Write("Please, enter the salary:");        // just to inform the user what they are going to enter
+            float salary = float.Parse(Console.ReadLine());    // it's recommended to store salary in float as user can enter floating-point number.
+            float netSalary = salary - (salary * 0.1f);        // it's equivalent to --->   float netSalary = salary * 0.9f;
+
+            Console.WriteLine("The salary before applying the tax = {0}", salary);
+            Console.WriteLine("The salary after applying the tax = {0}", netSalary);
+        }
+    }
+}
