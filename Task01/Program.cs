59af20c069909e761e5d5deebf96ed9ba1e7a7fe
Reham-Elbaz <50484754+Reham-Elbaz@users.noreply.github.com>
2022-10-24 23:30:07 +0200

    1st task

---
+++ Task01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+             * This program is to print my info 
+             * using 2 different ways of printing 
+             */
+
+            // The first way using WriteLine()
+            Console.WriteLine("I'm Reham.");
+            Console.WriteLine("I'm 22 years old.");
+            Console.WriteLine("I'm student at FCIS.");
+
+
+            // The second way using Write() and \n
+            Console.WriteLine("I'm Reham.\nI'm 22 years old.\nI'm student at FCIS.");
+        }
+    }
+}
